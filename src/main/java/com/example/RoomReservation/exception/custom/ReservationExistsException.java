cc8b0b0a11ced84ea38f44e5e2d62f02cf1a667d
David Stepanic <davidstepanic9@gmail.com>
2026-04-27 12:15:12 +0200

    Fix: Exception improvements

--- src/main/java/com/example/RoomReservation/exception/custom/ReservationExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.RoomReservation.exception.custom;
-
-public class ReservationExistsException extends RuntimeException {
-    public ReservationExistsException(String message) {
-        super(message);
-    }
-}
