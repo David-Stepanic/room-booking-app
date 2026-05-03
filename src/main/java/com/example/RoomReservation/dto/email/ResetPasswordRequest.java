c8ec0514ceabad36c29ec89c30b906604671539d
David Stepanic <davidstepanic9@gmail.com>
2026-05-03 12:14:06 +0200

    hotfix: update change/reset password validation

--- src/main/java/com/example/RoomReservation/dto/email/ResetPasswordRequest.java
+++ src/main/java/com/example/RoomReservation/dto/email/ResetPasswordRequest.java
@@ -1,6 +1,14 @@
 package com.example.RoomReservation.dto.email;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record ResetPasswordRequest(
         String token,
+        @NotBlank(message = "Password is required!")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)\\S{8,}$",
+                message = "Password must have at least 8 characters, one uppercase, one lowercase and one special character without whitespaces."
+        )
         String newPassword
 ) {}
--- src/main/java/com/example/RoomReservation/dto/user/PasswordRequest.java
+++ src/main/java/com/example/RoomReservation/dto/user/PasswordRequest.java
@@ -1,6 +1,7 @@
 package com.example.RoomReservation.dto.user;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
@@ -8,6 +9,10 @@ public class PasswordRequest {
     @NotBlank(message = "Old password is required!")
     private String oldPassword;
     @NotBlank(message = "New password is required!")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)\\S{8,}$",
+            message = "Password must have at least 8 characters, one uppercase, one lowercase and one special character without whitespaces."
+    )
     private String newPassword;
 
 }
