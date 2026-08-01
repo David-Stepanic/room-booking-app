642a6383a19a7680898ff31005883fb2f9750a1e
David <davidstepanic9@gmail.com>
2026-08-01 10:26:24 +0200

    Disabling integration test

--- backend/src/test/java/com/david/RoomReservation/RoomReservationApplicationTests.java
+++ backend/src/test/java/com/david/RoomReservation/RoomReservationApplicationTests.java
@@ -1,8 +1,10 @@
 package com.david.RoomReservation;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Temporarily disabled because project doesn't have integration test yet.")
 @SpringBootTest
 class RoomReservationApplicationTests {
 
